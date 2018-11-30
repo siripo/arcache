df4e7c8046fcf1cec1f0e335fc9675b715a9d721
Mariano Santamarina <msantamarina@gmail.com>
2018-11-30 01:42:58 -0300

    Add tests

--- arcache-core/src/main/java/ar/com/siripo/arcache/ArcacheClient.java
+++ arcache-core/src/main/java/ar/com/siripo/arcache/ArcacheClient.java
@@ -26,13 +26,13 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 
 	protected Random randomGenerator;
 
-	public ArcacheClient(ArcacheBackendClient backendClient) {
+	public ArcacheClient(final ArcacheBackendClient backendClient) {
 		this.backendClient = backendClient;
 		randomGenerator = new Random();
 	}
 
 	@Override
-	public void setDefaultOperationTimeout(long timeoutMillis) {
+	public void setDefaultOperationTimeout(final long timeoutMillis) {
 		if (timeoutMillis <= 0) {
 			throw new IllegalArgumentException();
 		}
@@ -45,7 +45,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setTimeMeasurementError(long errorSecs) {
+	public void setTimeMeasurementError(final long errorSecs) {
 		if (errorSecs < 0) {
 			throw new IllegalArgumentException();
 		}
@@ -58,7 +58,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setDefaultInvalidationWindow(long windowSecs) {
+	public void setDefaultInvalidationWindow(final long windowSecs) {
 		if (windowSecs < 0) {
 			throw new IllegalArgumentException();
 		}
@@ -71,7 +71,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setDefaultHardInvalidation(boolean hardInvalidation) {
+	public void setDefaultHardInvalidation(final boolean hardInvalidation) {
 		defaultHardInvalidation = hardInvalidation;
 	}
 
@@ -81,7 +81,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setKeyNamespace(String namespace) {
+	public void setKeyNamespace(final String namespace) {
 		keyNamespace = namespace;
 		if ("".equals(keyNamespace)) {
 			keyNamespace = null;
@@ -94,7 +94,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setKeyDelimiter(String keyDelimiter) {
+	public void setKeyDelimiter(final String keyDelimiter) {
 		if ((keyDelimiter == null) || (keyDelimiter.equals(""))) {
 			throw new IllegalArgumentException("The key delimiter must be a non empty String");
 		}
@@ -107,7 +107,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setDefaultExpirationTime(long expirationTimeSecs) {
+	public void setDefaultExpirationTime(final long expirationTimeSecs) {
 		if (expirationTimeSecs <= 0) {
 			throw new IllegalArgumentException();
 		}
@@ -120,7 +120,7 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void setDefaultStoredObjectRemovalTime(long removeTimeSecs) {
+	public void setDefaultStoredObjectRemovalTime(final long removeTimeSecs) {
 		if (removeTimeSecs <= 0) {
 			throw new IllegalArgumentException();
 		}
@@ -133,13 +133,13 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public Object get(String key) throws TimeoutException, Exception {
+	public Object get(final String key) throws TimeoutException, Exception {
 		return get(key, defaultOperationTimeoutMillis);
 	}
 
 	@Override
-	public Object get(String key, long timeoutMillis) throws TimeoutException, Exception {
-		CacheGetResult cacheGetResult = getCacheObject(key, timeoutMillis);
+	public Object get(final String key, final long timeoutMillis) throws TimeoutException, Exception {
+		final CacheGetResult cacheGetResult = getCacheObject(key, timeoutMillis);
 		switch (cacheGetResult.type) {
 		case HIT:
 			return cacheGetResult.value;
@@ -151,17 +151,17 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 		case INVALIDATED:
 			return null;
 		default:
-			return new IllegalStateException("Unhandled cacheResultType: " + cacheGetResult.type);
+			throw new IllegalStateException("Unhandled cacheResultType: " + cacheGetResult.type);
 		}
 	}
 
 	@Override
-	public CacheGetResult getCacheObject(String key) {
+	public CacheGetResult getCacheObject(final String key) {
 		return getCacheObject(key, defaultOperationTimeoutMillis);
 	}
 
 	@Override
-	public CacheGetResult getCacheObject(String key, long timeoutMillis) {
+	public CacheGetResult getCacheObject(final String key, final long timeoutMillis) {
 		try {
 			Future<CacheGetResult> getFuture = asyncGetCacheObject(key);
 			CacheGetResult r = getFuture.get(timeoutMillis, TimeUnit.MILLISECONDS);
@@ -172,7 +172,14 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 		} catch (TimeoutException tx) {
 			CacheGetResult err = new CacheGetResult(Type.TIMEOUT, tx);
 			return err;
-
+		} catch (ExecutionException ee) {
+			CacheGetResult err;
+			if (ee.getCause() instanceof Exception) {
+				err = new CacheGetResult(Type.ERROR, (Exception) ee.getCause());
+			} else {
+				err = new CacheGetResult(Type.ERROR, ee);
+			}
+			return err;
 		} catch (Exception e) {
 			CacheGetResult err = new CacheGetResult(Type.ERROR, e);
 			return err;
@@ -180,12 +187,30 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public void set(String key, Object value) throws TimeoutException, Exception {
+	public Future<CacheGetResult> asyncGetCacheObject(final String key) {
+		try {
+			if (key == null || key.equals("")) {
+				throw new IllegalArgumentException();
+			}
+			return buildCacheGetterTask(key);
+		} catch (Exception e) {
+			return DummyFuture.createWithException(e);
+		}
+	}
+
+	protected Future<CacheGetResult> buildCacheGetterTask(final String key) {
+		return new CacheGetterTask(key, backendClient, (BackendKeyBuilder) this,
+				(ArcacheConfigurationGetInterface) this, this.randomGenerator);
+	}
+
+	@Override
+	public void set(final String key, final Object value) throws TimeoutException, Exception {
 		set(key, value, null);
 	}
 
 	@Override
-	public void set(String key, Object value, String[] invalidationKeys) throws TimeoutException, Exception {
+	public void set(final String key, final Object value, final String[] invalidationKeys)
+			throws TimeoutException, Exception {
 		Future<Boolean> future = asyncSet(key, value, invalidationKeys);
 		try {
 			future.get(defaultOperationTimeoutMillis, TimeUnit.MILLISECONDS);
@@ -198,18 +223,16 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public Future<CacheGetResult> asyncGetCacheObject(String key) {
-		try {
-			return new CacheGetterTask(key, backendClient, (BackendKeyBuilder) this,
-					(ArcacheConfigurationGetInterface) this, this.randomGenerator);
-		} catch (Exception e) {
-			return DummyFuture.createWithException(e);
-		}
+	public Future<Boolean> asyncSet(final String key, final Object value) {
+		return asyncSet(key, value, null);
 	}
 
 	@Override
-	public Future<Boolean> asyncSet(String key, Object value, String[] invalidationKeys) {
+	public Future<Boolean> asyncSet(final String key, final Object value, final String[] invalidationKeys) {
 		try {
+			if (key == null || key.equals("")) {
+				throw new IllegalArgumentException();
+			}
 			ExpirableCacheObject expObj = new ExpirableCacheObject();
 			expObj.timestamp = System.currentTimeMillis() / 1000;
 			expObj.value = value;
@@ -225,27 +248,22 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public Future<Boolean> asyncSet(String key, Object value) {
-		return asyncSet(key, value, null);
-	}
-
-	@Override
-	public void invalidateKey(String key) throws TimeoutException, Exception {
+	public void invalidateKey(final String key) throws TimeoutException, Exception {
 		invalidateKey(key, defaultInvalidationWindowSecs);
 	}
 
 	@Override
-	public void invalidateKey(String key, long invalidationWindowSecs) throws TimeoutException, Exception {
+	public void invalidateKey(final String key, final long invalidationWindowSecs) throws TimeoutException, Exception {
 		invalidateKey(key, defaultHardInvalidation, invalidationWindowSecs);
 	}
 
 	@Override
-	public void invalidateKey(String key, boolean hardInvalidation) throws TimeoutException, Exception {
+	public void invalidateKey(final String key, final boolean hardInvalidation) throws TimeoutException, Exception {
 		invalidateKey(key, hardInvalidation, defaultInvalidationWindowSecs);
 	}
 
 	@Override
-	public void invalidateKey(String key, boolean hardInvalidation, long invalidationWindowSecs)
+	public void invalidateKey(final String key, final boolean hardInvalidation, final long invalidationWindowSecs)
 			throws TimeoutException, Exception {
 		Future<Boolean> future = asyncInvalidateKey(key, hardInvalidation, invalidationWindowSecs);
 		try {
@@ -259,7 +277,8 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	@Override
-	public Future<Boolean> asyncInvalidateKey(String key, boolean hardInvalidation, long invalidationWindowSecs) {
+	public Future<Boolean> asyncInvalidateKey(final String key, final boolean hardInvalidation,
+			final long invalidationWindowSecs) {
 		try {
 			if (invalidationWindowSecs < 0) {
 				throw new IllegalArgumentException();
@@ -280,14 +299,14 @@ public class ArcacheClient implements ArcacheClientInterface, BackendKeyBuilder 
 	}
 
 	/** Create the key to be used in the backend client */
-	public String createBackendKey(String userKey) {
+	public String createBackendKey(final String userKey) {
 		if (keyNamespace == null) {
 			return userKey;
 		}
 		return keyNamespace + keyDelimiter + userKey;
 	}
 
-	public String createInvalidationBackendKey(String invalidationKey) {
+	public String createInvalidationBackendKey(final String invalidationKey) {
 		return createBackendKey(invalidationKeyPrefix + keyDelimiter + invalidationKey);
 	}
 
---
+++ arcache-core/src/test/java/ar/com/siripo/arcache/ArcacheReadWriteInterfaceTest.java
@@ -0,0 +1,414 @@
+package ar.com.siripo.arcache;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.com.siripo.arcache.backend.ArcacheBackendClient;
+import ar.com.siripo.arcache.backend.test.ArcacheInMemoryTestBackend;
+import ar.com.siripo.arcache.util.DummyFuture;
+
+public class ArcacheReadWriteInterfaceTest {
+
+	ArcacheInMemoryTestBackend backendClient;
+	BuildCacheGetterTaskInterceptor rinterceptor;
+	ArcacheReadWriteInterface rInterface;
+	ArcacheConfigurationInterface rconfig;
+
+	@Before
+	public void setUp() throws Exception {
+		backendClient = new ArcacheInMemoryTestBackend();
+		rinterceptor = new BuildCacheGetterTaskInterceptor(backendClient);
+		rInterface = rinterceptor;
+		rconfig = rinterceptor;
+	}
+
+	@Test
+	public void testGetString() throws Exception {
+		Object readedv;
+
+		rInterface.set("juan", "algo");
+		rinterceptor.resetInterceptor();
+		readedv = rInterface.get("juan");
+		rinterceptor.assertIntercepted("juan");
+		assertEquals(readedv, "algo");
+
+		// Test timeout propagation
+		ArcacheClient clix = new ArcacheClient(backendClient) {
+			@Override
+			public Object get(final String key, final long timeoutMillis) throws TimeoutException, Exception {
+				return "" + timeoutMillis;
+			}
+		};
+
+		clix.setDefaultOperationTimeout(456);
+		assertEquals(clix.get("key"), "456");
+	}
+
+	@Test
+	public void testGetStringLong() throws Exception {
+		Object readedv;
+
+		rInterface.set("juan", "algo");
+		rinterceptor.resetInterceptor();
+		readedv = rInterface.get("juan", 111);
+		rinterceptor.assertIntercepted("juan");
+		assertEquals(readedv, "algo");
+
+		readedv = rInterface.get("Not found key", 111);
+		assertEquals(readedv, null);
+
+		try {
+			rInterface.get("TimeoutException", 111);
+			fail();
+		} catch (TimeoutException e) {
+		}
+
+		try {
+			rInterface.get("InterruptedException", 111);
+			fail();
+		} catch (InterruptedException e) {
+		}
+
+		try {
+			rInterface.get(null, 111);
+			fail();
+		} catch (IllegalArgumentException e) {
+		}
+
+		// Walk all types testing if all are interpreted
+		for (final CacheGetResult.Type typetest : CacheGetResult.Type.values()) {
+			ArcacheClient cli = new ArcacheClient(backendClient) {
+				@Override
+				public CacheGetResult getCacheObject(final String key, final long timeoutMillis) {
+					return new CacheGetResult(typetest, new InterruptedException());
+				}
+			};
+
+			try {
+				cli.get("key", 22);
+			} catch (InterruptedException e) {
+			}
+		}
+
+		// Test expired
+		ArcacheClient clix = new ArcacheClient(backendClient) {
+			@Override
+			public CacheGetResult getCacheObject(final String key, final long timeoutMillis) {
+				CacheGetResult cr = new CacheGetResult(CacheGetResult.Type.EXPIRED);
+				cr.value = "xx";
+				return cr;
+			}
+		};
+
+		assertNull(clix.get("key", 22));
+
+		// Test Invalidated
+		ArcacheClient clii = new ArcacheClient(backendClient) {
+			@Override
+			public CacheGetResult getCacheObject(final String key, final long timeoutMillis) {
+				CacheGetResult cr = new CacheGetResult(CacheGetResult.Type.INVALIDATED);
+				cr.value = "xx";
+				return cr;
+			}
+		};
+
+		assertNull(clii.get("key", 22));
+
+	}
+
+	@Test
+	public void testGetCacheObjectString() throws Exception {
+		CacheGetResult cgr;
+
+		rInterface.set("juan", "algo");
+		rinterceptor.resetInterceptor();
+		cgr = rInterface.getCacheObject("juan");
+		rinterceptor.assertIntercepted("juan");
+		assertEquals(cgr.value, "algo");
+		assertTrue(cgr.isHit());
+
+		// Test timeout propagation
+
+		ArcacheClient clix = new ArcacheClient(backendClient) {
+			@Override
+			public CacheGetResult getCacheObject(final String key, final long timeoutMillis) {
+				CacheGetResult cr = new CacheGetResult(CacheGetResult.Type.HIT);
+				cr.value = "" + timeoutMillis;
+				return cr;
+			}
+		};
+
+		clix.setDefaultOperationTimeout(334);
+		assertEquals(clix.getCacheObject("key").value, "334");
+	}
+
+	@Test
+	public void testGetCacheObjectStringLong() throws Exception {
+
+		CacheGetResult cgr;
+
+		rInterface.set("juan", "algo");
+		rinterceptor.resetInterceptor();
+		cgr = rInterface.getCacheObject("juan", 22);
+		rinterceptor.assertIntercepted("juan");
+		assertEquals(cgr.value, "algo");
+		assertTrue(cgr.isHit());
+
+		cgr = rInterface.getCacheObject("NULL", 22);
+		rinterceptor.assertIntercepted("NULL");
+		assertEquals(cgr.value, null);
+		assertEquals(cgr.type, CacheGetResult.Type.ERROR);
+		assertTrue(cgr.cause instanceof NullPointerException);
+
+		cgr = rInterface.getCacheObject("TimeoutException", 22);
+		assertEquals(cgr.type, CacheGetResult.Type.TIMEOUT);
+		assertTrue(cgr.cause instanceof TimeoutException);
+
+		cgr = rInterface.getCacheObject("", 22);
+		assertEquals(cgr.type, CacheGetResult.Type.ERROR);
+		assertTrue(cgr.cause instanceof IllegalArgumentException);
+
+		cgr = rInterface.getCacheObject("ExecutionException", 22);
+		assertEquals(cgr.type, CacheGetResult.Type.ERROR);
+		assertTrue(cgr.cause instanceof ExecutionException);
+
+		cgr = rInterface.getCacheObject("InterruptedException", 22);
+		assertEquals(cgr.type, CacheGetResult.Type.ERROR);
+		assertTrue(cgr.cause instanceof InterruptedException);
+
+		cgr = rInterface.getCacheObject("IllegalStateException", 22);
+		assertEquals(cgr.type, CacheGetResult.Type.ERROR);
+		assertTrue(cgr.cause instanceof IllegalStateException);
+
+	}
+
+	@Test
+	public void testAsyncGetCacheObject() throws Exception {
+		rinterceptor.resetInterceptor();
+		rInterface.asyncGetCacheObject("juan");
+		rinterceptor.assertIntercepted("juan");
+
+		Future<CacheGetResult> future = rInterface.asyncGetCacheObject("");
+		try {
+			future.get();
+			fail();
+		} catch (ExecutionException e) {
+			assertTrue(e.getCause() instanceof IllegalArgumentException);
+		}
+
+		future = rInterface.asyncGetCacheObject(null);
+		try {
+			future.get();
+			fail();
+		} catch (ExecutionException e) {
+			assertTrue(e.getCause() instanceof IllegalArgumentException);
+		}
+
+		future = rInterface.asyncGetCacheObject("IllegalStateException");
+		try {
+			future.get();
+			fail();
+		} catch (ExecutionException e) {
+			assertTrue(e.getCause() instanceof IllegalStateException);
+
+		}
+
+		future = rInterface.asyncGetCacheObject("TimeoutException");
+		try {
+			future.get(1, TimeUnit.HOURS);
+			fail();
+		} catch (TimeoutException e) {
+			assertTrue(e instanceof TimeoutException);
+
+		}
+
+		future = rInterface.asyncGetCacheObject("InterruptedException");
+		try {
+			future.get();
+			fail();
+		} catch (InterruptedException e) {
+			assertTrue(e instanceof InterruptedException);
+
+		}
+
+		future = rInterface.asyncGetCacheObject("ExecutionException");
+		try {
+			future.get();
+			fail();
+		} catch (ExecutionException e) {
+			assertTrue(e instanceof ExecutionException);
+
+		}
+	}
+
+	@Test
+	public void testSetStringObject() throws Exception {
+		final ArcacheClient cli = new ArcacheClient(backendClient) {
+			public void set(final String key, final Object value, final String[] invalidationKeys)
+					throws TimeoutException, Exception {
+				assertEquals(key, "key");
+				assertEquals(value, "val");
+				assertNull(invalidationKeys);
+			}
+		};
+		cli.set("key", "val");
+	}
+
+	@Test
+	public void testSetStringObjectStringArray() throws Exception {
+		// Functional
+		rInterface.set("key", "val", null);
+		assertEquals(rInterface.get("key"), "val");
+
+		try {
+			rInterface.set(null, "val", null);
+		} catch (IllegalArgumentException ex) {
+
+		}
+
+		// White Box
+		final ArcacheClient cli = new ArcacheClient(backendClient) {
+			public Future<Boolean> asyncSet(final String key, final Object value, final String[] invalidationKeys) {
+				return new DummyFuture<Boolean>(null) {
+					@Override
+					public Boolean get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException {
+						throw new ExecutionException(new Error());
+					}
+				};
+			}
+		};
+
+		try {
+			cli.set(null, "val", null);
+		} catch (ExecutionException xx) {
+			assertTrue(xx.getCause() instanceof Error);
+		}
+	}
+
+	@Test
+	public void testAsyncSetStringObject() {
+		final ArcacheClient cli = new ArcacheClient(backendClient) {
+			public Future<Boolean> asyncSet(final String key, final Object value, final String[] invalidationKeys) {
+				assertEquals(key, "key");
+				assertEquals(value, "val");
+				assertNull(invalidationKeys);
+				return null;
+			}
+		};
+		cli.asyncSet("key", "val");
+	}
+
+	@Test
+	public void testAsyncSetStringObjectStringArray() throws Exception {
+		// Functional
+		rInterface.asyncSet("key", "val", null).get();
+		assertEquals(rInterface.get("key"), "val");
+
+		// Functional Exception
+		try {
+			rInterface.asyncSet(null, "val", null).get();
+		} catch (ExecutionException ex) {
+			assertTrue(ex.getCause() instanceof IllegalArgumentException);
+		}
+
+		try {
+			rInterface.asyncSet("", "val", null).get();
+		} catch (ExecutionException ex) {
+			assertTrue(ex.getCause() instanceof IllegalArgumentException);
+		}
+
+		// White Box test
+		final ArcacheClient cliorig = new ArcacheClient(backendClient);
+
+		ArcacheBackendClient backendClientx = new ArcacheInMemoryTestBackend() {
+			@Override
+			public Future<Boolean> asyncSet(String key, int ttlSeconds, Object value) {
+				ExpirableCacheObject expObj = (ExpirableCacheObject) value;
+				assertEquals(cliorig.createBackendKey("key"), key);
+				assertEquals(Math.max(2, Math.abs(expObj.timestamp - System.currentTimeMillis() / 1000)), 2);
+				assertEquals(expObj.value, "val");
+				assertArrayEquals(expObj.invalidationKeys, new String[] { "a", "b" });
+				assertEquals(ttlSeconds, 555);
+				return null;
+			}
+		};
+		ArcacheClient clix = new ArcacheClient(backendClientx);
+		clix.setDefaultStoredObjectRemovalTime(555);
+
+		clix.asyncSet("key", "val", new String[] { "a", "b" });
+
+		// Test handle Exception
+		ArcacheBackendClient backendClientEx = new ArcacheInMemoryTestBackend() {
+			@Override
+			public Future<Boolean> asyncSet(String key, int ttlSeconds, Object value) {
+				throw new IllegalStateException();
+			}
+		};
+		ArcacheClient cliEx = new ArcacheClient(backendClientEx);
+
+		Future<Boolean> fut = cliEx.asyncSet("key", "val", new String[] {});
+		try {
+			fut.get();
+			fail();
+		} catch (ExecutionException ex) {
+			assertTrue(ex.getCause() instanceof IllegalStateException);
+		}
+	}
+
+	static class BuildCacheGetterTaskInterceptor extends ArcacheClient {
+
+		String key;
+		boolean called;
+
+		public BuildCacheGetterTaskInterceptor(ArcacheBackendClient backendClient) {
+			super(backendClient);
+			resetInterceptor();
+		}
+
+		@Override
+		protected Future<CacheGetResult> buildCacheGetterTask(final String key) {
+			this.key = key;
+			this.called = true;
+
+			if (key == "IllegalStateException") {
+				throw new IllegalStateException();
+			}
+			if (key == "TimeoutException") {
+				return DummyFuture.createWithException(new TimeoutException());
+			}
+			if (key == "InterruptedException") {
+				return DummyFuture.createWithException(new InterruptedException());
+			}
+			if (key == "ExecutionException") {
+				return DummyFuture.createWithException(new ExecutionException(new Error()));
+			}
+
+			if (key == "NULL") {
+				return new DummyFuture<CacheGetResult>(null);
+			}
+
+			return super.buildCacheGetterTask(key);
+		}
+
+		public void resetInterceptor() {
+			this.key = null;
+			this.called = false;
+		}
+
+		public void assertIntercepted(String key) {
+			assertTrue(this.called);
+			assertEquals(this.key, key);
+		}
+	}
+}
