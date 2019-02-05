dd7c59d4eca2ecfc74ed832529e9c7a98f03b786
Mariano Santamarina <mariano.santamarina@mercadolibre.com>
2019-02-05 20:15:17 -0300

    Fix ArcacheClientFactoryBean Test Coverage

--- arcache-core/src/test/java/ar/com/siripo/arcache/spring/ArcacheClientFactoryBeanTest.java
+++ arcache-core/src/test/java/ar/com/siripo/arcache/spring/ArcacheClientFactoryBeanTest.java
@@ -2,23 +2,33 @@ package ar.com.siripo.arcache.spring;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.Future;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import ar.com.siripo.arcache.ArcacheClient;
+import ar.com.siripo.arcache.backend.ArcacheBackendClient;
 
 public class ArcacheClientFactoryBeanTest {
 
 	ArcacheClientFactoryBean factoryBean;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		factoryBean=new ArcacheClientFactoryBean();
+		factoryBean = new ArcacheClientFactoryBean();
 	}
 
 	@Test
 	public void testAfterPropertiesSet() throws Exception {
 		factoryBean.afterPropertiesSet();
+	}
+
+	@Test
+	public void testDestroy() throws Exception {
+		assertTrue(factoryBean.getObject() instanceof ArcacheClient);
+		factoryBean.destroy();
+		assertEquals(factoryBean.getObject(), null);
 	}
 
 	@Test
@@ -28,7 +38,7 @@ public class ArcacheClientFactoryBeanTest {
 
 	@Test
 	public void testGetObjectType() {
-		assertTrue(factoryBean.getObjectType() == ArcacheClient.class);
+		assertEquals(factoryBean.getObjectType(), ArcacheClient.class);
 	}
 
 	@Test
@@ -39,7 +49,70 @@ public class ArcacheClientFactoryBeanTest {
 	@Test
 	public void testSetDefaultOperationTimeout() throws Exception {
 		factoryBean.setDefaultOperationTimeout(20);
-		assertEquals(factoryBean.getObject().getDefaultOperationTimeout(),20);
+		assertEquals(factoryBean.getObject().getDefaultOperationTimeout(), 20);
+	}
+
+	@Test
+	public void testSetTimeMeasurementError() throws Exception {
+		factoryBean.setTimeMeasurementError(21);
+		assertEquals(factoryBean.getObject().getTimeMeasurementError(), 21);
+	}
+
+	@Test
+	public void testSetDefaultInvalidationWindow() throws Exception {
+		factoryBean.setDefaultInvalidationWindow(22);
+		assertEquals(factoryBean.getObject().getDefaultInvalidationWindow(), 22);
+	}
+
+	@Test
+	public void testSetDefaultHardInvalidation() throws Exception {
+		factoryBean.setDefaultHardInvalidation(true);
+		assertEquals(factoryBean.getObject().getDefaultHardInvalidation(), true);
+		factoryBean.setDefaultHardInvalidation(false);
+		assertEquals(factoryBean.getObject().getDefaultHardInvalidation(), false);
+	}
+
+	@Test
+	public void testSetKeyNamespace() throws Exception {
+		factoryBean.setKeyNamespace("AbC");
+		assertEquals(factoryBean.getObject().getKeyNamespace(), "AbC");
+	}
+
+	@Test
+	public void testSetKeyDelimiter() throws Exception {
+		factoryBean.setKeyDelimiter("()");
+		assertEquals(factoryBean.getObject().getKeyDelimiter(), "()");
+	}
+
+	@Test
+	public void testSetDefaultExpirationTime() throws Exception {
+		factoryBean.setDefaultExpirationTime(51);
+		assertEquals(factoryBean.getObject().getDefaultExpirationTime(), 51);
+	}
+
+	@Test
+	public void testSetDefaultStoredObjectRemovalTime() throws Exception {
+		factoryBean.setDefaultStoredObjectRemovalTime(52);
+		assertEquals(factoryBean.getObject().getDefaultStoredObjectRemovalTime(), 52);
+	}
+
+	@Test
+	public void testSetBackendClient() throws Exception {
+		ArcacheBackendClient mbc = new ArcacheBackendClient() {
+			public Future<Boolean> asyncSet(String key, int ttlSeconds, Object value) {
+				return null;
+			}
+
+			public Future<Object> asyncGet(String key) {
+				return null;
+			}
+		};
+
+		factoryBean.setBackendClient(mbc);
+		assertEquals(factoryBean.getObject().getBackendClient(), mbc);
+
+		mbc.asyncSet(null, 1, 0);
+		mbc.asyncGet(null);
 	}
 
 }
